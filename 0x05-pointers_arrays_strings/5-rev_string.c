6b0b28a568ca9d74ca63847be21646473fe874cc
Ifiok Inuk <ifiokinuk1@gmail.com>
2023-03-27 13:39:31 -0700

    puts

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * rev_string - This fuction print a string reverser
+ * rev_string - reverses a string
+ * @s: holi
  *
- *@s: this is the pointer that point to a string
  */
 void rev_string(char *s)
 {
-	int a, len;
-	char *begin, *end = s;
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+	int i;
+	int j;
+	int temp;
+	int length;
+	while (s[i] != 0)
 	{
-		end++;
+		i++;
 	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
+	length = i;
+	i = 0;
+	j = length - 1;
+	while (i < j)
 	{
-		char x;
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
 	}
-	end[len + 1] = '\0';
 }
